72e67200b526598b1f7fa7139e55fab081e77b05
cyylog <cyylog@163.com>
2022-03-17 00:33:05 +0800

    2022年03月17日  0:33:05

--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
-func main()  {
-	r:=gin.Default()
-	r.GET("/v1/topic", func(c *gin.Context) {
-		if c.Query("username")==""{
-			c.String(300,"获取帖子列表")
-		}else {
-			c.String(200,"获取用户=%s的帖子列表",c.Param("username"))
-		}
-	})
-	r.GET("/v1/topics/:topic_id", func(c *gin.Context) {
-		c.String(200,"获取topicid=%s的帖子",c.Param("topic_id"))
-	})
 
-	r.Run(":8080")
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK,"hello word")
+	})
+r.Run(":8000")
 }
