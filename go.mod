1d46b83e6eae686d088b26acdc366fa18afea58e
cyylog <cyylog@163.com>
2022-03-19 00:48:16 +0800

    crud 2022年03月19日  0:48:14

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module gin_study
 
-go 1.16
+go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.7.7
--- main.go
+++ main.go
@@ -6,29 +6,124 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-
-
 var db *sql.DB
 
-func initDB() (err error) {
-	dns := "root:root@tcp(127.0.0.1:3306)/cyylog"
-	db, err := sql.Open("mysql", dns)
-	if err != nil {
-		return err
-	}
-	err= db.Ping()
-	if err!=nil{
-		return err
-	}
-	defer db.Close()
-	return err
+type user struct {
+	id   int
+	age  int
+	name string
 }
 
-func main() {
-	err:=initDB()
-	if err!=nil{
-		fmt.Println("init db failed , err: %v\n",err)
+func initMySQL() (err error) {
+	dns := "root:root@tcp(127.0.0.1:3306)/cyylog"
+	db, err = sql.Open("mysql", dns)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Printf("connect to db failed,err: %v\n", err)
 		return
 	}
+	//db.SetConnMaxLifetime(time.Second * 10)
+	db.SetMaxOpenConns(200)
+	db.SetMaxIdleConns(10)
+	return
+}
 
+// 查询单条数据示例
+func queryRowDemo() {
+	sqlStr := "select id, name, age from user where id=?"
+	var u user
+	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
+	err := db.QueryRow(sqlStr, 3).Scan(&u.id, &u.name, &u.age)
+	if err != nil {
+		fmt.Printf("scan failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+}
+
+// 查询多条数据示例
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from user where id > ?"
+	rows, err := db.Query(sqlStr, 0)
+	if err != nil {
+		fmt.Printf("query failed, err:%v\n", err)
+		return
+	}
+	// 非常重要：关闭rows释放持有的数据库链接
+	defer rows.Close()
+
+	// 循环读取结果集中的数据
+	for rows.Next() {
+		var u user
+		err := rows.Scan(&u.id, &u.name, &u.age)
+		if err != nil {
+			fmt.Printf("scan failed, err:%v\n", err)
+			return
+		}
+		fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+	}
+}
+
+// 插入数据
+func insertRowDemo() {
+	sqlStr := "insert into user(name, age) values (?,?)"
+	ret, err := db.Exec(sqlStr, "王五", 38)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return
+	}
+	theID, err := ret.LastInsertId() // 新插入数据的id
+	if err != nil {
+		fmt.Printf("get lastinsert ID failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is %d.\n", theID)
+}
+
+// 更新数据
+func updateRowDemo() {
+	sqlStr := "update user set age=? where id = ?"
+	ret, err := db.Exec(sqlStr, 39, 3)
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("update success, affected rows:%d\n", n)
+}
+
+// 删除数据
+func deleteRowDemo() {
+	sqlStr := "delete from user where id = ?"
+	ret, err := db.Exec(sqlStr, 3)
+	if err != nil {
+		fmt.Printf("delete failed, err:%v\n", err)
+		return
+	}
+	n, err := ret.RowsAffected() // 操作影响的行数
+	if err != nil {
+		fmt.Printf("get RowsAffected failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("delete success, affected rows:%d\n", n)
+}
+func main() {
+
+	if err := initMySQL(); err != nil {
+		fmt.Printf("connect to db failed,err: %v\n", err)
+	}
+	defer db.Close()
+	fmt.Printf("connect to db success")
+	//queryRowDemo()
+	//queryMultiRowDemo()
+	//insertRowDemo()
+	//updateRowDemo()
+	deleteRowDemo()
 }
